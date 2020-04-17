ba3214648459e02c686904f6ce4ec5aa19303e98
unknown <hesham14yahia@gmail.com>
2020-04-17 02:22:35 +0200

    init_commit

---
+++ src/ImageFullControl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hesham14Yahia\ImageFullControl;
+
+
+class ImageFullControl
+{
+
+    public function uploadImage()
+    {
+        return "ImageFullControl";
+    }
+}
---
+++ src/ImageFullControlServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hesham14Yahia\ImageFullControl;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageFullControlServiceProvider extends ServiceProvider
+{
+    public function boot()
+    { }
+
+    public function register()
+    { }
+}
